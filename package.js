b9f77bb726b62996fef1e0e1ac0a2716e1c23016
Kelly <copleykj@gmail.com>
2015-05-27 23:30:16 -0400

    use and imply packages
    Since almost all of the same packages are being added for each of
    the socialize packages, we can just add and imply them in the
    base-model and they will be available to packages that add base-model

--- package.js
+++ package.js
@@ -7,7 +7,19 @@ Package.describe({
 Package.onUse(function(api) {
     api.versionsFrom("1.0.2.1");
 
-    api.use(["meteor", "underscore"]);
+    api.use(["meteor", "mongo", "underscore"]);
+
+    api.use([
+        "socialize:server-time@0.1.1", "tmeasday:publish-with-relations@0.2.0", "aldeed:simple-schema@1.3.2",
+        "aldeed:collection2@2.3.3", "matb33:collection-hooks@0.7.13", "meteorhacks:unblock@1.1.0"
+    ]);
+
+    api.imply(["meteor", "mongo", "underscore"]);
+
+    api.imply([
+        "tmeasday:publish-with-relations@0.2.0", "aldeed:simple-schema@1.3.2",
+        "aldeed:collection2@2.3.3", "matb33:collection-hooks@0.7.13", "meteorhacks:unblock@1.1.0"
+    ]);
 
     api.addFiles("base-model.js");
 
