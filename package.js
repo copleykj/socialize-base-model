18b833b90fa2d473ec26a72f1e9b71b96e8e4ac4
Kelly <copleykj@gmail.com>
2015-06-09 09:47:02 -0400

    bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "socialize:base-model",
     summary: "A model for all other models to inherit from ",
-    version: "0.2.0",
+    version: "0.2.1",
 });
 
 Package.onUse(function(api) {
