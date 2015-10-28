6ca72009ea485f2e12725916bc5c9854311f1da2
Abigail Watson <awatson1978@gmail.com>
2015-10-27 22:15:18 -0700

    moved to clinical-meteor org; bump major version

--- package.js
+++ package.js
@@ -1,29 +1,37 @@
 Package.describe({
-    name: "awatson1978:base-model",
-    summary: "A model for all other models to inherit from ",
-    version: "0.3.0",
-    git: "https://github.com/awatson1978/socialize-base-model.git"
+  name: "clinical:base-model",
+  summary: "A model for all other models to inherit from ",
+  version: "1.3.0",
+  git: "https://github.com/clinical-meteor/clinical-base-model.git"
 });
 
-Package.onUse(function(api) {
-    api.versionsFrom("1.0.2.1");
+Package.onUse(function (api) {
+  api.versionsFrom('1.1.0.2');
 
-    api.use(["meteor", "mongo", "underscore"]);
+  api.use([
+    "meteor",
+    "mongo",
+    "underscore"
+  ]);
 
-    api.use([
-        "socialize:server-time@0.1.1", "tmeasday:publish-with-relations@0.2.0", "aldeed:simple-schema@1.3.3",
-        "aldeed:collection2@2.3.3", "matb33:collection-hooks@0.7.13"
+  api.use([
+      "socialize:server-time@0.1.1",
+      "tmeasday:publish-with-relations@0.2.0",
+      "aldeed:simple-schema@1.3.3",
+      "aldeed:collection2@2.3.3",
+      "matb33:collection-hooks@0.7.13"
     ]);
 
-    api.imply(["meteor", "mongo", "underscore"]);
+  api.imply(["meteor", "mongo", "underscore"]);
 
-    api.imply([
-        "tmeasday:publish-with-relations@0.2.0", "aldeed:simple-schema@1.3.3",
-        "aldeed:collection2@2.3.3", "matb33:collection-hooks@0.7.13"
+  api.imply([
+      "tmeasday:publish-with-relations@0.2.0",
+      "aldeed:simple-schema@1.3.3",
+      "aldeed:collection2@2.3.3",
+      "matb33:collection-hooks@0.7.13"
     ]);
 
-    api.addFiles(["base-model.js", "security.js"]);
+  api.addFiles(["base-model.js", "security.js"]);
 
-    api.export("BaseModel");
+  api.export("BaseModel");
 });
-
