19f6fb3a5f1337ea64b939f4b210c5c3fa2b61ef
StorytellerCZ <jxd3455@rit.edu>
2016-02-02 23:08:20 -0500

    Updated dependencies and added improvements from the clinical track
    Updated dependencies
    Changes from the clinical track
    Changed the package.js to be back to socialize
    tmeasday:publish-with-relations -> lepozepo:publish-with-relations
    matb33:collection-hooks@0.8.1 -> artpolikarpov:collection-hooks@0.8.5

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: "clinical:base-model",
+  name: "socialize:base-model",
   summary: "A model for all other models to inherit from ",
-  version: "1.3.0",
-  git: "https://github.com/clinical-meteor/clinical-base-model.git"
+  version: "0.4.0",
+  git: "https://github.com/copleykj/socialize-base-model.git"
 });
 
 Package.onUse(function (api) {
@@ -15,20 +15,20 @@ Package.onUse(function (api) {
   ]);
 
   api.use([
-      "socialize:server-time@0.1.1",
-      "tmeasday:publish-with-relations@0.2.0",
-      "aldeed:simple-schema@1.3.3",
-      "aldeed:collection2@2.3.3",
-      "matb33:collection-hooks@0.7.13"
+      "socialize:server-time@0.1.2",
+      "lepozepo:publish-with-relations@1.2.4",
+      "aldeed:simple-schema@1.5.3",
+      "aldeed:collection2@2.8.0",
+      "matb33:collection-hooks@0.8.1"
     ]);
 
   api.imply(["meteor", "mongo", "underscore"]);
 
   api.imply([
-      "tmeasday:publish-with-relations@0.2.0",
-      "aldeed:simple-schema@1.3.3",
-      "aldeed:collection2@2.3.3",
-      "matb33:collection-hooks@0.7.13"
+      "lepozepo:publish-with-relations@1.2.4",
+      "aldeed:simple-schema@1.5.3",
+      "aldeed:collection2@2.8.0",
+      "matb33:collection-hooks@0.8.1"
     ]);
 
   api.addFiles(["base-model.js", "security.js"]);
