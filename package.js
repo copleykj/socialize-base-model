08465312f932ff9667f515c218469309e8e0ba8e
copleykj <copleykj@gmail.com>
2016-02-12 11:21:40 -0500

    patch version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "socialize:base-model",
     summary: "A model for all other models to inherit from ",
-    version: "0.3.1",
+    version: "0.3.2",
     git: "https://github.com/copleykj/socialize-base-model.git"
 });
 
