87ebd3ea4596e4285519b84a818ddd68d78b847a
Kelly Copley <copleykj@gmail.com>
2017-07-13 16:27:08 -0400

    Fix issue with MessageBox causing a Buffer is not defined on client.

--- security.js
+++ security.js
@@ -1,15 +1,22 @@
 /* eslint-disable import/no-unresolved */
+import { Meteor } from 'meteor/meteor';
 import SimpleSchema from 'simpl-schema';
 import MessageBox from 'message-box';
 
-
-MessageBox.defaults({
-    messages: {
-        en: {
-            Untrusted: 'Inserts/Updates from untrusted code not supported',
+/* We check for server code here to deal with a buffer issue in meteor-message-box
+ * This shouldn't be a major issue as I doubt we will need to display this error
+ * on the client at this point. Should be fixed though.
+ * https://github.com/aldeed/meteor-message-box/issues/1
+*/
+if (Meteor.isServer) {
+    MessageBox.defaults({
+        messages: {
+            en: {
+                Untrusted: 'Inserts/Updates from untrusted code not supported',
+            },
         },
-    },
-});
+    });
+}
 
 SimpleSchema.denyUntrusted = function denyUntrusted() {
     if (this.isSet) {
