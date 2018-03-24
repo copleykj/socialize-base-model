d77b65f0751c264e52d7e75f91cbb0f2db00998f
Kelly Copley <copleykj@gmail.com>
2018-03-24 18:18:05 -0400

    use custom react-native-meteor and bump version for publish

--- entry-rn.js
+++ entry-rn.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-unresolved */
-import Meteor from 'react-native-meteor';
+import Meteor from '@socialize/react-native-meteor';
 import construct from './base-model.js';
 /* eslint-enable import/no-unresolved */
 
