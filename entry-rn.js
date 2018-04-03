e0c81b59dd436b29a47e468496acf25006912b63
Kelly Copley <copleykj@gmail.com>
2018-04-03 12:44:08 -0400

    import @socialize/react-native-meteor-collection2 so that it modifies Collection

--- entry-rn.js
+++ entry-rn.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-unresolved */
 import Meteor from '@socialize/react-native-meteor';
+import '@socialize/react-native-meteor/collection2';
 import construct from './base-model.js';
 /* eslint-enable import/no-unresolved */
 
